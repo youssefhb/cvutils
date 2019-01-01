dbe84fc0fa8a882925e374113772f8025ebce5af
youssefhb <youssef.hbali@gmail.com>
2019-01-01 18:15:47 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Computer Vision Utils',
+    packages=['cvutils', 'cvutils.video', 'cvutils.io','cvutils.features'],
+    version='1.0.0',
+    description='Computer Vision Utils',
+    author='Youssef Hbali',
+    author_email='youssef.hbali@gmail.com',
+    url='https://github.com/youssefhb/cvutils',
+    download_url='',
+    keywords=['computer vision', 'Pattern recognition'],
+    classifiers=[],
+    scripts=['bin'],
+)
