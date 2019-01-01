0fe6aaa97e0a84dd5af2a1b46d769a5701b2d366
youssefhb <youssef.hbali@gmail.com>
2019-01-01 19:31:31 +0100

    Python 2.7 & 3

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-for req in $(cat requirements.txt); do pip install $req; done
+# Python 2.7
+for req in $(cat requirements.txt); do pip2.7 install $req; done
+
+# Python 3
+#for req in $(cat requirements.txt); do pip install $req; done
