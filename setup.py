801772bb16951fa15b97427a68d17bda4404a9f3
youssefhb <youssef.hbali@gmail.com>
2019-01-05 17:40:52 +0100

    New packages added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Computer Vision Utils',
-    packages=['cvutils', 'cvutils.video', 'cvutils.io','cvutils.features'],
+    packages=['cvutils','cvutils.dl','cvutils.features','cvutils.image','cvutils.io','cvutils.video','cvutils.visualization'],
     version='1.0.0',
     description='Computer Vision Utils',
     author='Youssef Hbali',
