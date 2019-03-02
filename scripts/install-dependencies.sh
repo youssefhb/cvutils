076012d6ef2ddadc26131f5beecd97d554b609db
youssefhb <youssef.hbali@gmail.com>
2019-03-02 22:10:13 +0100

    adding sudo to the command

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Python 2.7
-for req in $(cat requirements.txt); do pip2.7 install $req; done
+for req in $(cat requirements.txt); do sudo pip2.7 install $req; done
 
 # Python 3
 #for req in $(cat requirements.txt); do pip install $req; done
